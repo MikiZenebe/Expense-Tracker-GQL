029555bf34af7e157300d4fb7261c9aee3858557
MikiZenebe <mickyaszenebe@gmail.com>
2024-04-22 10:43:33 -0500

    updated

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
+import express from "express";
+import http from "http";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+//Ensure we wait for our server to start
+await server.start();
 
 console.log(`🚀 Server ready at ${url}`);
